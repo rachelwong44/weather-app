fc87e9acf8430a4397ba8d91665dd154fab34bd4
Rachel Wong <rachelwong4444@gmail.com>
2023-08-22 10:32:34 +0100

    date-builder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import './App.css';
-import React, { useEffect, useState } from "react";
+import React, { useState } from 'react';
+
 export default function App() {
 
-  const [lat, setLat] = useState([]);
-  const [long, setLong] = useState([]);
+  const api = {
+    key: "9adfc103f31154764e8d5d054a68b28f    ",
+    base: "https://api.openweathermap.org/data/2.5/"
+  }
 
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      setLat(position.coords.latitude);
-      setLong(position.coords.longitude);
-    });
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-    console.log("Latitude is:", lat)
-    console.log("Longitude is:", long)
-  }, [lat, long]);
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
 
   return (
-    <div className="App">
 
+    <div className="App">
+      <main>
+        <div className="search-box">
+          <input 
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            
+          />
+        </div>
+        <div>
+          <div className="location-box">
+            <div className="location">New York City, US</div>
+            <div className="date">{dateBuilder(new Date())}</div>
+          </div>
+          </div>
+      </main>
     </div>
   );
 }
